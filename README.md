8fec8ef45becbebb0602142ec6c3692d340140b4
Dintaparekh <dintaparekh.595@gmail.com>
2020-06-21 16:09:14 -0400

    Change kayout

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # dintaparekh.github.io/
 This is my Website (Github Page) which would give a brief Introduction about me and overall summary of my professional career track.
+
+You can check it here https://dintaparekh.github.io/
--- static/src/js/index.js
+++
@@ -1,9 +0,0 @@
-document.getElementsByClassName('dp_header_action_btn_container')[0].addEventListener('click', (ev) => {
-    if (ev.target.dataset.id) {
-        window.open(`#${ev.target.dataset.id}`, '_self');
-    }
-})
-
-function openLink(link) {
-    window.open(link, '_blank');
-}
